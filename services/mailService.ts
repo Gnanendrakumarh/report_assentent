8c29ecdf158b5da3160676cf3681a331c36bdd1a
Gnanendrakumarh <gnanendrakumarh@gmail.com>
2025-09-18 19:18:21 +0530

    cup namdu

--- services/mailService.ts
+++ services/mailService.ts
@@ -3,12 +3,24 @@ import nodemailer from "nodemailer";
 import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
+import fetch from "node-fetch";
 
 dotenv.config({ path: ".env.local" });
 
+
+
 const app = express();
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
+
+
+
+app.use(express.static(path.join(__dirname, "../dist")));
+app.get("*", (_, res) => {
+  res.sendFile(path.resolve(__dirname, "../dist/index.html"));
+});
 
 // Plain object for Status (replaces enum)
 const Status = {
@@ -171,7 +183,7 @@ app.post("/send-whatsapp", async (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(` Mail + WhatsApp server running on port ${PORT}`);
 });
