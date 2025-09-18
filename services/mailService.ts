b97891353309c40e4844bab3cbfca156dc7db260
Gnanendrakumarh <gnanendrakumarh@gmail.com>
2025-09-18 17:26:18 +0530

    v6

--- services/mailService.ts
+++ services/mailService.ts
@@ -172,8 +172,8 @@ app.post("/send-whatsapp", async (req, res) => {
 
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(` Mail + WhatsApp server running on port ${PORT}`);
 });
+
 
