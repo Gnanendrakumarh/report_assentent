de23896a212300dfc1fa6c3b5f5a9e3d5afb5156
Gnanendrakumarh <gnanendrakumarh@gmail.com>
2025-09-18 19:08:38 +0530

    e sala cup namde

--- dist-server/types.js
+++
@@ -1,6 +0,0 @@
-export var Status;
-(function (Status) {
-    Status["Completed"] = "Completed";
-    Status["InProgress"] = "In Progress";
-    Status["NoProgress"] = "No Progress";
-})(Status || (Status = {}));
